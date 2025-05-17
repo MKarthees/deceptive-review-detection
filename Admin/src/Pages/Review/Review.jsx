c83298e7880a964aad5c7b566885027a534b5ac0
MKarthees <kartheeswaran68@gmail.com>
2025-05-17 12:03:03 +0530

    Review page

--- Admin/src/Pages/Review/Review.jsx
+++ Admin/src/Pages/Review/Review.jsx
@@ -1,16 +1,43 @@
-import React, { useContext } from "react";
-import { DataContext } from "../../Context/Context";
-import "./review.css";
+import React, { useContext } from 'react'
+import axios from 'axios'
+import { DataContext } from '../../Context/Context'
+import './review.css'
+import { backendURL } from '../../App'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const Review = () => {
-  const { slider, setSlider } = useContext(DataContext);
+  const { slider, setSlider } = useContext(DataContext)
+  const [review, setReview] = useState([])
+  const [allReview, setAllReview] = useState([])
+  const [reviewId, setReviewId] = useState('')
+  const eachReview = async () => {
+    const response = await axios.get(backendURL + '/review/list')
+    setAllReview(response.data.reviews)
+    allReview.map((item) => {
+      setReviewId(item.productId)
+    })
+  }
+  useEffect(() => {
+    eachReview()
+  }, [allReview])
+
   return (
-    <main className={`${slider == true ? "container" : "grid"}`}>
+    <main className={`${slider == true ? 'container' : 'grid'}`}>
       <section className="review">
-        <h1>Review Page</h1>
+        <div className="review-container">
+          <h1>Reviews</h1>
+
+          {allReview.map((item, index) => (
+            <div className="reviews" key={index}>
+              <h4>{item.name}</h4>
+              <p>{item.review}</p>
+            </div>
+          ))}
+        </div>
       </section>
     </main>
-  );
-};
+  )
+}
 
-export default Review;
+export default Review
